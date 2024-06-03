fc52b37cabcfe4d13b8cd85e1318f7e2f1f9688d
Hilmee15 <hilstore75@gmail.com>
2024-06-03 09:24:11 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if (Auth::check())
+        {
+            $usertype = Auth::user()->usertype;
+
+            if ($usertype == 'user')
+            {
+                return view('dashboard');
+            }
+            else if ($usertype == 'admin')
+            {
+                return view('admin.dashboard');
+            }
+            else
+            {
+                return redirect()->back();
+            }
+        }
+        else
+        {
+            return redirect('login');
+        }
+    }
+    public function post() {
+        return view('admin.post');
+    }
+}
+
